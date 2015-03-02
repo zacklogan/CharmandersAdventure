df1b885e3f0ee385518d2cb94d9f7725cae8ae82
zacklogan <bigred9921@mchsi.com>
2015-03-02 13:18:20 -0600

    Game File

---
+++ Charmanders Adventure.bat
@@ -0,0 +1,623 @@
+:start
+
+cls
+
+@echo off
+
+echo Welcome to Charmander, and friends' quest!
+
+echo ----------------------------------
+
+echo To begin ,press: B
+
+echo ----------------------------------
+
+echo To see the credits , press: C
+
+echo ----------------------------------
+
+
+
+
+SET Choice=
+SET /P Choice=Choose credits, or begin and press Enter
+IF NOT '%Choice%'== SET Choice=%Choice:~0, 1%
+IF /I '%Choice%'=='c' GOTO :credits
+IF /I '%Choice%'=='b' GOTO :begin
+ECHO.
+
+echo
+
+:credits
+
+cls
+
+@echo off
+
+echo Made by Penny Brumbaugh, Or "The Dragon Gamer/Artist" on youtube
+
+
+echo ---------------------------------------------------------------------
+
+
+
+
+pause
+
+goto :start
+
+
+
+
+
+:begin
+
+cls
+
+@echo off
+
+echo choose your Pokemon! (viewing profiles IS reccomended!!!)
+
+echo -----------------------------------------------------------------
+
+echo Charmander-Gender,F (to view profile press: v , To choose Charmander press C)
+
+SET Choice=
+SET /P Choice=Choose your pokemon and press enter
+IF NOT '%Choice%'== SET Choice=%Choice:~0, 1%
+IF /I '%Choice%'=='V' GOTO :VeiwProfile
+IF /I '%Choice%'=='C' GOTO :ChooseCharmander
+
+echo
+
+:VeiwProfile
+
+cls
+
+@echo off
+
+echo Charmander- Charmander is a very offensive, yet elegant fire type who won't 
+
+echo stop at any needs to help her friends or family, she has a soft spot for grass 
+
+echo and bug types, disregarding their weaknesses, and loves to fight, Charmander is a leader, not a follower, so keep that in mind before you make a mistake!
+
+echo ------------------------------------------------------------------------------
+
+echo Starting Moves: Ember, scratch,growl.
+
+echo -----------------------------------------
+
+echo weaknesses- Water, Ground, Rock.
+
+echo ---------------------------------------
+
+echo Strong against- Bug, Grass, Steel, Ice.
+
+echo ---------------------------------------------
+
+
+
+pause
+
+goto :begin
+
+
+echo
+
+:ChooseCharmander
+
+cls
+
+@echo off
+
+echo You have chosen Charmander!
+
+pause
+
+cls
+
+echo You hear the alarm clock blaring in your ears
+
+pause
+
+echo ???:"ugh..huh?!" You look at the blaring clock that reads 7:20 am, you realize 
+
+echo that it is time for school.
+
+pause
+
+cls 
+
+echo You turn off the alarm clock and go downstairs to get ready for the first
+
+echo day of seventh grade.
+
+pause
+
+cls
+
+echo On the way out of the door, you think of skipping school.....
+
+pause
+
+cls
+
+echo Press G to Go to school, Press S to skip school
+
+SET Choice=choose to go to school or to skip school and press enter
+SET /P Choice=Choose to go to school
+IF NOT '%Choice%'== SET Choice=%Choice:~0, 1%
+IF /I '%Choice%'=='G' GOTO :Go
+IF /I '%Choice%'=='S' GOTO :skip
+
+
+echo
+
+:Go
+
+cls
+
+
+echo You:"huh, maybe I should go to school, besides..my parents will kill me if
+
+echo I didn't..." 
+
+pause
+
+cls
+
+echo You start make your way to the school bus.
+
+pause
+
+cls
+
+echo but when you get halfway to the bus, a gruff-looking squirtle blocks your way, 
+
+echo pushes you back and you fall down.
+
+pause
+
+cls
+
+echo The sqirtle jumps in the bus, glancing at you with a smirk,
+
+echo and then the bus drives away.
+
+pause
+
+cls
+
+echo You try to shoot an ember, but its too late.
+
+pause
+
+Goto :Skip
+
+echo
+
+:Skip
+
+cls
+
+echo You decide not to go to school, and you take a little detour into the forest.
+
+pause
+
+cls
+
+echo You: "Let's see...if I go this way I can make it to the new arcade-"
+
+pause
+
+cls
+
+echo you get inturrupted by tripping over something
+
+pause
+
+cls
+
+echo you get back up on your feet and see a green silhouette scampering away into 
+
+echo a rawst berry bush.
+
+pause
+
+cls
+
+echo You:"huh, who's there?!" You look around worried
+
+pause
+
+cls
+
+echo Nothing, nor no one shows themselves
+
+pause
+
+cls
+
+echo You:" I said....WHO'S THERE?! Show yourself,before I make you!" 
+
+echo You get ready to fight
+
+pause
+
+cls
+
+echo you start to see a small,shaking,green foot steps out of the Rawst berry bush.
+
+pause
+
+cls
+
+echo You panic and shoot an ember at the creature's foot, it fell down, and rolled
+
+echo out of the bush, it was in pain.
+
+pause
+
+cls
+
+echo You walk up to it, to see a Bulbasaur writhing in pain on the ground,the
+
+echo fire spreading from his foot to the rest of his body.
+
+pause
+
+cls
+
+echo You:"oh no.." you feel terrible for hurting an innocent pokemon, you look at
+
+echo the rawst berry bush and grab one.
+
+pause
+
+cls
+
+echo You:"Here! take this rawst berry!" You feed it to the bulbasaur,
+
+echo and surely,the burn goes away.
+
+echo You see that the bulbasaur has fainted.
+
+pause
+
+cls
+
+echo You, having a soft spot for grass types, picks up the bulbasaur 
+
+echo and makes it over to a nearby lake, with lots of different berry trees
+
+echo growing next to it.
+
+pause 
+
+cls
+
+echo you mix some berries and herbs to heal up the hurt Bulbasaur as good as you can.
+
+echo you use the medicine.
+
+pause
+
+cls
+
+echo The Bulbasaur wakes up, looks around and sees you sitting down nest to him.
+
+pause
+
+cls
+
+echo The Bulbasaur jumped up and the scrambled backwards,fell down over a tree root,
+
+echo got back up,brought out his vines and grabs you.
+
+pause
+
+cls
+
+echo you feel yourself getting lifted up into the air and look down to see
+
+echo that the Bulbasaur has woken up and is holding you in the air.
+
+pause
+
+cls
+
+echo Bulbasaur:"W-who are you and why are you here?" He said in a shakey voice.
+
+echo Bulbasaur:"And why do I have burn marks on me?!" He added 
+
+pause
+
+cls
+
+echo You:"I-uh,-"
+
+pause
+
+cls
+
+echo Bulbasaur:"If you came to attack me, I'll-I'll Throw you into this river over
+
+echo here!" 
+
+pause 
+
+cls
+
+echo You scramble for words, each time you failed to say the sentence, you got 
+
+echo pulled closer and closer to the water.
+
+echo You feel your tail fire toutch the water, you start to loose consciousness
+
+echo until you can't even talk anymore.
+
+pause 
+
+cls
+
+echo The Bulbasaur notices that your tail fire is getting low, in his nature
+
+echo he sets you down on the ground.
+
+pause
+
+cls
+
+echo Bulbasaur:"I need an answer." he calms down a little bit
+
+pause
+
+cls
+
+echo You:"I....Thought you were going to attack me...and I panicked and used ember,
+
+echo then I saw that you didn't mean harm, I felt terrible so I healed your burns."
+
+echo........
+
+echo You "well...most of them..."
+
+pause
+
+cls
+
+echo You:"I'm so sorry...can you ever forgive me...?" You say weakly
+
+pause
+
+cls
+
+echo Bulbasaur took back his vines, and you fall on the ground, exhausted.
+
+pause
+
+cls
+
+echo Bulbasaur helps you stand up with his vines.
+
+echo Bulbasaur: Are you ok?
+
+echo You: Oh, yeah I'm fine, I...just....my tail fire is starting to go out from
+
+echo the lake..."
+
+pause
+
+cls
+
+echo Bulbasaur leads you to his little den made out of leaves and branches,
+
+echo and lets you rest in it and regain your fire.
+
+echo Bulbasaur walks to the far corner and takes a rest of his own.
+
+pause
+
+cls
+
+echo A few hours later
+
+pause
+
+cls
+
+echo You wake up, refreshed, and see that bulbasaur is nowhere to be seen.
+
+pause
+
+cls
+
+echo You walk out of the den to see Bulbasaur gathering multiple berries and
+
+echo placing them in a hand woven basket.
+
+pause
+
+cls
+
+echo you walk over to him.
+
+echo You"Hey...." you say not knowing his name
+
+pause
+
+cls
+
+echo Bulbasaur:"My name's Bulbasaur, but you can call me 'Bulby'" he turns to you
+
+echo and answers in a shy tone.
+
+pause
+
+cls
+
+echo You:"I was wondering if you wanted to come to the arcade with me,because I'm 
+
+echo going to head there."
+
+pause
+
+cls
+
+echo Bulby:"uh...I don't usually..go outside of this forest..." he said in a 
+
+echo slightly frightened tone.
+
+pause 
+
+cls
+
+echo You:"well, why not?"
+
+echo Bulby:"since I don't have anyone to raise me...I like to stay away from other
+
+echo pokemon outside this forest as possible...thats why I ran away from you.."
+
+echo You:"oh.."
+
+pause
+
+cls
+
+echo You:"But I promise you'll be ok, I'll make sure of it!"
+
+pause
+
+cls
+
+echo Bulby:"uh..er..ok, I'll trust you on this, its time to leave my comfort zone
+
+echo anyways."
+
+pause
+
+cls
+
+echo Bulby has joined the team!
+
+pause 
+
+cls
+
+echo You and Bulby enter the arcade
+
+pause 
+
+cls
+
+echo Bulby:"I've heard of video games from some of my neighbors in this forest,
+
+echo but I've never seen one before...." 
+
+pause 
+
+cls
+
+@echo off
+
+cls
+
+echo You think of which arcade to pick.....
+
+
+
+echo To choose to play Space invaders, press: s, to choose Donkey Kong, press:d,
+
+echo to play Tetris, press:t
+
+SET Choice=
+SET /P Choice=Choose an arcade game and press enter
+IF NOT '%Choice%'== SET Choice=%Choice:~0, 1%
+IF /I '%Choice%'=='s' GOTO :SpaceInvaders
+IF /I '%Choice%'=='d' GOTO :donkeyKong 
+IF /I '%Choice%'=='t' GOTO :Tetris 
+
+echo
+
+:Spaceinvaders
+
+cls
+
+
+echo You walk over to the arcade and put in a quarter.
+
+pause 
+
+cls
+
+echo You now have $59.75
+
+pause
+
+cls
+
+echo 00000000000000000000000000000000000000000000000000000000000000000000000000000
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo. 
+echo 00000000000000000000000000000000000000000000000000000000000000000000000000000
+
+Pause
+
+cls
+
+echo
+
+:DonkeyKong
+
+cls
+
+echo You walk over to the arcade and put in a quarter.
+
+pause
+
+cls
+
+
+echo You now have $59.75
+
+
+
+
+
+
+
+
+pause
+
+echo
+
+:Tetris
+
+cls
+
+echo Not done
+
+pause
+
+
+
+
+
+
